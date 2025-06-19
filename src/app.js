5b247bf5f893ce3c5eb3ff796e5a9a1b0074409a
Aye Lecman <ayelenlecman@gmail.com>
2025-06-19 00:47:28 +0000

    modificacion a funciones

--- src/app.js
+++ src/app.js
@@ -12,12 +12,14 @@ window.onload = function() {
   let what = ['my homework', 'my phone', 'the car'];
   let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
   
+  let bloques = [who, action, what, when];
+
+  function generarRandom (array) {
+    return array[Math.floor(Math.random() * array.length)]
+  }
+
   function generateExcuse() {
-    let randomWho = who[Math.floor(Math.random() * who.length)];
-    let randomAction = action[Math.floor(Math.random() * action.length)];
-    let randomWhat = what[Math.floor(Math.random() * what.length)];
-    let randomWhen = when[Math.floor(Math.random() * when.length)];
-    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+    return bloques.map(generarRandom).join(' ');
   }
 
   document.getElementById("excuse").innerText = generateExcuse();
